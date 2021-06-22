bf471299c849b01e1dd3e4a54e3c9388fc6b1642
Bruno Duyé <brunetton@brunetton.org>
2021-06-22 20:20:11 +0200

    Major version: use youtube-dl "-f bestaudio" to only download audio + support other sources than Youtube

--- youtube-crawler.rb
+++ youtube-crawler.rb
@@ -11,7 +11,9 @@ Usage:
 #{__FILE__} --from-file <filename> [--numbering]
 
 Options:
-  --numbering:    add a number at begining of mp3 files (ex: 01 - blabla.mp3)
+  --numbering        add a number at begining of mp3 files (ex: 01 - blabla.mp3)
+  --metadata -m      (only if Youtube url) add a text file containing video description
+  --youtubedl_args   custom additional Youtube-dl args
 
 Convert given video link, or all video links found in given file to mp3, using youtube-dl and avconv.
 Audio level normalized using loudnorm audio filter (http://ffmpeg.org/ffmpeg-all.html#loudnorm)
@@ -19,36 +21,47 @@ Audio level normalized using loudnorm audio filter (http://ffmpeg.org/ffmpeg-all
 DOCOPT
 
 YOUTUBE_LINK_REGEXP = /(https?:\/\/.*youtube.*\/watch\?v=([^#\&\?\s]+))/
+YOUTUBEDL_DEFAULT_ARGS = "-f bestaudio --no-playlist --console-title"
+FFMPEG_OPTIONS = "-c:a mp3 -filter:a loudnorm=i=-18:lra=17 -qscale:a 2"
 
-def treat_url(url, video_id, audio_file_number=nil)
+def treat_url(url, audio_file_number=nil)
 
   def make_filename_complient(title)
     return title.gsub('/','-').gsub('`','\'')
   end
 
-  # Open url
-  page = Nokogiri::HTML(open(url))
-  video_description = page.css('p[id="eow-description"]').inner_html.gsub('<br>', "\n")
-  video_title = page.css('h1[class="watch-title-container"]').text.strip
-  print "#{video_id}: #{video_title}\n\n#{video_description}\n\n"
-  # Save text
-  txt_filename = "#{make_filename_complient(video_title)}-#{video_id}"
-  File.open(txt_filename + '.txt', 'w') { |file| file.write(video_description) }
+  # Check youtube url and extract parts (clean url and video id)
+  if match = url.match(YOUTUBE_LINK_REGEXP)  # TODO: allow video ID
+    url, video_id = match.captures
+    if $args['metadata']
+      # Open youtube link and get text informations
+      page = Nokogiri::HTML(open(url))
+      video_description = page.css('p[id="eow-description"]').inner_html.gsub('<br>', "\n")
+      video_title = page.css('h1[class="watch-title-container"]').text.strip
+      print "#{video_id}: #{video_title}\n\n#{video_description}\n\n"
+      # Save text
+      txt_filename = "#{make_filename_complient(video_title)}-#{video_id}"
+      File.open(txt_filename + '.txt', 'w') { |file| file.write(video_description) }
+    end
+  end
 
-  # Download file
-  command = "youtube-dl \"#{url}\""
+  # Call youtube-dl to download sound
+  youtubedl_args = YOUTUBEDL_DEFAULT_ARGS
+  youtubedl_args += ' ' + $args['youtubedl_args'] if $args['youtubedl_args']
+  command = "youtube-dl #{youtubedl_args} \"#{url}\""
   puts "--> Running \"#{command}\""
   system(command)
   if $?.exitstatus != 0
       exit(-1)
   end
 
-  # Convert to mp3
-  video_file = Dir.glob("*#{video_id}*.*").find{|filename| not filename.end_with? '.txt'}  # Retreive file by video_id
-  puts "\n\n--> Converting '#{video_file}' to mp3"
+  # Convert to mp3 and normalize audio
+  ## Find the last file by date in directory
+  input_file = Dir.glob("*").find_all{|filename| not filename.end_with? '.txt' and File.file?(filename) }.map{|filename| [filename, File.stat(filename).ctime]}.max_by{|e| e[1]}.first
+  puts "\n\n--> Converting '#{input_file}' to mp3 and normalize audio loudness"
   number = audio_file_number ? "%02d - " % audio_file_number : ''
-  mp3_filename = number + File.basename(make_filename_complient(video_file), '.*') + '.mp3'
-  command = "ffmpeg -i \"#{video_file}\" -c:a mp3 -filter:a loudnorm=i=-18:lra=17 -qscale:a 2 \"#{mp3_filename}\""
+  mp3_filename = number + File.basename(make_filename_complient(input_file), '.*') + '.mp3'
+  command = "ffmpeg -i \"#{input_file}\" #{FFMPEG_OPTIONS} \"#{mp3_filename}\""
   puts "Running \"#{command}\""
   system(command)
   if $?.exitstatus != 0
@@ -61,36 +74,30 @@ def treat_url(url, video_id, audio_file_number=nil)
   end
 
   # remove video file
-  File.delete(video_file)
+  File.delete(input_file)
 end
 
 
 begin
-  args = Docopt::docopt(doc)
+  $args = Docopt::docopt(doc)
 rescue Docopt::Exit => e
   puts e.message
   exit
 end
 
-if args['<url>']
-  url = args['<url>']
-  # Check youtube url
-  if match = url.match(YOUTUBE_LINK_REGEXP)  # TODO: allow video ID
-    url, video_id = match.captures
-  else
-    abort("\"#{url}\" is not a valid youtube url !")
-  end
-  treat_url(url, video_id)
+if $args['<url>']
+  url = $args['<url>']
+  treat_url(url)
   puts "\n\n--> End"
-elsif args['<filename>']
+elsif $args['<filename>']
   file_number = 0
-  File.readlines(args['<filename>']).each do |line|
+  File.readlines($args['<filename>']).each do |line|
     if match = line.match(YOUTUBE_LINK_REGEXP)
       file_number += 1
-      url, video_id = match.captures
+      url = match.captures
       puts "\n\n***** #{url}\n"
-      number = args['--numbering'] ? file_number : nil
-      treat_url(url, video_id, number)
+      number = $args['--numbering'] ? file_number : nil
+      treat_url(url, number)
     end
   end
   puts "\n\nEnd\n#{file_number} files treated."

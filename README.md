3abb14af7cd27a2f95236db60b93a582ed2c19fd
Bruno Duyé <brunetton@brunetton.org>
2021-06-23 00:31:49 +0200

    Major version: use youtube-dl to call ffmpeg to normalize volume

--- README.md
+++ README.md
@@ -1,29 +1,19 @@
 # Youtube albums crawler
 
-**UPDATE**: since recent versions of youtube-dl, almost all this script could be replaced by this simple command:
-
-```
-youtube-dl -f bestaudio "https://youtu.be/..." --exec "ffmpeg -i {} -c:a mp3 -filter:a loudnorm=i=-18:lra=17 -qscale:a 2 {}.mp3 && rm {} "
-```
-
-You can remove the `--no-playlist` option if you want to download full playlist (if the youtube URL refers to a video and a playlist)
-
 ## What is it ?
 
 The aim of this project is:
-  * to download and convert to mp3 full albums found in youtube
+  * to download and convert to mp3 full albums found in youtube, facebook or other sources handled by youtube-dl
   * to download all videos found in a text file, as mp3
   * to harmonize sound loudness between different sources (using [ffmpeg loudnorm audio filter](http://ffmpeg.org/ffmpeg-all.html#loudnorm))
 
 It relays on the excellent [youtube-dl](https://rg3.github.io/youtube-dl/) and the non less excellent [avconv](https://libav.org/avconv.html).
 
-## Options
+It actually calls youtube-dl with a command line like this one:
 
-  * urls can be grepped from a text file containing urls (lines that are not youtube urls are ignored)
+    youtube-dl -f bestaudio --exec "ffmpeg -i {} -c:a mp3 -filter:a loudnorm=i=-18:lra=17 -qscale:a 2 {}.mp3 && rm {}" [url]
 
-## Limitations
-
-For now the script only download video, converts it to mp3 and save video description in a txt file for future treatments (see TODO section).
+Where [url] is the given video url.
 
 ## Installation
 
@@ -31,21 +21,20 @@ For now the script only download video, converts it to mp3 and save video descri
 
 sudo aptitude install youtube-dl ffmpeg
 sudo gem install docopt
-sudo gem install nokogiri
 
 ### Arch Linux
 
 pacman -S youtube-dl ffmpeg
 sudo gem install docopt
-sudo gem install nokogiri
 
 ## Usage
 
     youtube-crawler.rb <youtube_video_url>
     youtube-crawler.rb --from-file <filename> [--numbering]
 
-  --from-file   treat all youtube urls found in given text file (max one per line). Urls can be "hidden" in the middle of some text.
-  --numbering   add a number before each mp3 filenames. Ex: (ex: `01 - blabla-jYq1X_fJ9.mp3`)
+  * `--from-file`   treat all youtube urls found in given text file (max one per line). Urls can be "hidden" in the middle of some text.
+  * `--numbering`   add a number before each mp3 filenames. Ex: (ex: `01 - blabla-jYq1X_fJ9.mp3`)
+  * use `--youtubedl-args "--yes-playlist"` if you want to download full playlist (if the youtube URL refers to a video and a playlist)
 
 ## TODO
 
--- youtube-crawler.rb
+++ youtube-crawler.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
 require "docopt"
 
 doc = <<DOCOPT
@@ -11,9 +9,8 @@ Usage:
 #{__FILE__} --from-file <filename> [--numbering]
 
 Options:
-  --numbering        add a number at begining of mp3 files (ex: 01 - blabla.mp3)
-  --metadata -m      (only if Youtube url) add a text file containing video description
-  --youtubedl_args   custom additional Youtube-dl args
+  --numbering                 add a number at begining of mp3 files (ex: 01 - blabla.mp3)
+  --youtubedl-args <args>     custom additional Youtube-dl args
 
 Convert given video link, or all video links found in given file to mp3, using youtube-dl and avconv.
 Audio level normalized using loudnorm audio filter (http://ffmpeg.org/ffmpeg-all.html#loudnorm)
@@ -21,60 +18,16 @@ Audio level normalized using loudnorm audio filter (http://ffmpeg.org/ffmpeg-all
 DOCOPT
 
 YOUTUBE_LINK_REGEXP = /(https?:\/\/.*youtube.*\/watch\?v=([^#\&\?\s]+))/
-YOUTUBEDL_DEFAULT_ARGS = "-f bestaudio --no-playlist --console-title"
+YOUTUBEDL_DEFAULT_ARGS = "-f bestaudio --no-playlist --console-title --exec 'ffmpeg -i {} -c:a mp3 -filter:a loudnorm=i=-18:lra=17 -qscale:a 2 {}.mp3 && rm {}'"
 FFMPEG_OPTIONS = "-c:a mp3 -filter:a loudnorm=i=-18:lra=17 -qscale:a 2"
 
 def treat_url(url, audio_file_number=nil)
-
-  def make_filename_complient(title)
-    return title.gsub('/','-').gsub('`','\'')
-  end
-
-  # Check youtube url and extract parts (clean url and video id)
-  if match = url.match(YOUTUBE_LINK_REGEXP)  # TODO: allow video ID
-    url, video_id = match.captures
-    if $args['metadata']
-      # Open youtube link and get text informations
-      page = Nokogiri::HTML(open(url))
-      video_description = page.css('p[id="eow-description"]').inner_html.gsub('<br>', "\n")
-      video_title = page.css('h1[class="watch-title-container"]').text.strip
-      print "#{video_id}: #{video_title}\n\n#{video_description}\n\n"
-      # Save text
-      txt_filename = "#{make_filename_complient(video_title)}-#{video_id}"
-      File.open(txt_filename + '.txt', 'w') { |file| file.write(video_description) }
-    end
-  end
-
-  # Call youtube-dl to download sound
-  youtubedl_args = YOUTUBEDL_DEFAULT_ARGS
-  youtubedl_args += ' ' + $args['youtubedl_args'] if $args['youtubedl_args']
-  command = "youtube-dl #{youtubedl_args} \"#{url}\""
+  # Call youtube-dl to download sound and call ffmpeg
+  youtubedl-args = YOUTUBEDL_DEFAULT_ARGS
+  youtubedl-args += ' ' + $args['youtubedl-args'] if $args['youtubedl-args']
+  command = "youtube-dl #{youtubedl-args} \"#{url}\""
   puts "--> Running \"#{command}\""
   system(command)
-  if $?.exitstatus != 0
-      exit(-1)
-  end
-
-  # Convert to mp3 and normalize audio
-  ## Find the last file by date in directory
-  input_file = Dir.glob("*").find_all{|filename| not filename.end_with? '.txt' and File.file?(filename) }.map{|filename| [filename, File.stat(filename).ctime]}.max_by{|e| e[1]}.first
-  puts "\n\n--> Converting '#{input_file}' to mp3 and normalize audio loudness"
-  number = audio_file_number ? "%02d - " % audio_file_number : ''
-  mp3_filename = number + File.basename(make_filename_complient(input_file), '.*') + '.mp3'
-  command = "ffmpeg -i \"#{input_file}\" #{FFMPEG_OPTIONS} \"#{mp3_filename}\""
-  puts "Running \"#{command}\""
-  system(command)
-  if $?.exitstatus != 0
-      exit(-1)
-  end
-
-  # Check mp3 file exists and > 0
-  if not (File.exist?(mp3_filename) and File.size(mp3_filename) > 0)
-      abort("OOPS, mp3 file doesn't seems to be generated -> abording")
-  end
-
-  # remove video file
-  File.delete(input_file)
 end
 
 
